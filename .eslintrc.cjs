8a985c8dd715f0ed81faff8cd4d5aa46dc9c443c
cedricbou <cedric.boufflers@gmail.com>
2024-02-03 01:08:31 +0100

    fix(lint): fixed typescript linting setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: { browser: true, es6: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@vue/typescript/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
