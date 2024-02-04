52bc1b44685deab0a9f14c1014451582feea89f6
cedricbou <cedric.boufflers@gmail.com>
2024-02-04 23:10:58 +0100

    feat(start): Added a cube with rotating stars and orbit controls

--- src/components/ShaderScreen.vue
+++ src/components/ShaderScreen.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
 import { onMounted, Ref, ref } from 'vue';
+
+/**
+ * Import THREE, the 3D library we are going to use.
+ */
 import * as THREE from 'three';
-import { rendererInit } from './render/ThreeRenderer';
+import * as ADDONS from 'three/addons';
+
+/**
+ * Import fp-ts, a functional programming library for TypeScript.
+ * We are going to use it to handle errors and to chain.
+ */
 import * as E from 'fp-ts/lib/Either';
 import * as F from 'fp-ts/lib/function';
 
+/**
+ * TODO : challenge the usefulness of this lib.
+ */
+import { rendererInit } from './render/ThreeRenderer';
+
+/**
+ * Import our shader code from assets, as string.
+ */
 import fragmentShader from '../assets/shaders/starrycube.frag.glsl?raw';
 import vertexShader from '../assets/shaders/starrycube.vert.glsl?raw';
 
 const sceneParams = {
   cubeRotationStep: new THREE.Quaternion().setFromAxisAngle(
     new THREE.Vector3().random(),
-    0.01,
+    0.0,
   ),
-  cubeTranslationStep: 0.015,
+  cubeTranslationStep: 0.0,
   cubeTranslationRange: 4,
 };
 
@@ -22,6 +39,7 @@ type SceneParams = typeof sceneParams;
 type SceneState = {
   cubeRotationCurrent: THREE.Quaternion;
   cubeTranslationCurrent: number;
+  time: number;
 };
 
 const props = defineProps<{
@@ -31,6 +49,26 @@ const props = defineProps<{
 const shaderScreen: Ref<HTMLCanvasElement | undefined> = ref();
 const errorMsg: Ref<string | undefined> = ref();
 
+const createOrbitControl = function (
+  camera: THREE.Camera,
+): ADDONS.OrbitControls {
+  const orbitControl = new ADDONS.OrbitControls(camera, shaderScreen.value);
+  orbitControl.enableDamping = true;
+  orbitControl.dampingFactor = 0.25;
+  orbitControl.enableZoom = true;
+  return orbitControl;
+};
+
+const createStageGrid = function (): THREE.GridHelper {
+  const grid = new THREE.GridHelper(
+    30,
+    30,
+    THREE.Color.NAMES.yellowgreen,
+    THREE.Color.NAMES.teal,
+  );
+  return grid;
+};
+
 const createCamera = function (renderer: THREE.WebGLRenderer): THREE.Camera {
   const size = renderer.getSize(new THREE.Vector2());
   const camera = new THREE.PerspectiveCamera(
@@ -39,15 +77,26 @@ const createCamera = function (renderer: THREE.WebGLRenderer): THREE.Camera {
     0.1,
     1000,
   );
-  camera.position.z = 8;
+  camera.position.z = 10;
+  camera.position.y = 5;
+  camera.position.x = 3;
+  camera.lookAt(0, 0, 0);
   return camera;
 };
 
-const createCube = function (): THREE.Mesh {
+const createUniforms = function (): { [key: string]: THREE.IUniform } {
+  return {
+    iTime: { value: 0.0 },
+  };
+};
+
+const createCube = function (uniforms: {
+  [key: string]: THREE.IUniform;
+}): THREE.Mesh {
   const geometry = new THREE.BoxGeometry(3.5, 3.5, 3.5);
   // const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
   const shaderMaterial = new THREE.RawShaderMaterial({
-    uniforms: {},
+    uniforms: uniforms,
     vertexShader,
     fragmentShader,
     glslVersion: THREE.GLSL3,
@@ -64,20 +113,23 @@ const createLight = function (): THREE.Light {
 
 const createScene = function ({
   camera,
+  grid,
   light,
   cube,
 }: {
   camera: THREE.Camera;
+  grid: THREE.GridHelper;
   light: THREE.Light;
   cube: THREE.Mesh;
 }): THREE.Scene {
-  return new THREE.Scene().add(camera).add(light).add(cube);
+  return new THREE.Scene().add(camera).add(grid).add(light).add(cube);
 };
 
 const initState = function (cube: THREE.Mesh): SceneState {
   return {
     cubeRotationCurrent: cube.quaternion,
     cubeTranslationCurrent: 0,
+    time: performance.now(),
   };
 };
 
@@ -99,6 +151,7 @@ const animate = function ({
   scene,
   camera,
   cube,
+  uniforms,
 }: {
   state: SceneState;
   params: SceneParams;
@@ -106,8 +159,11 @@ const animate = function ({
   scene: THREE.Scene;
   camera: THREE.Camera;
   cube: THREE.Mesh;
+  uniforms: { [key: string]: THREE.IUniform };
 }) {
   updateState(state, params);
+
+  uniforms.iTime.value = (performance.now() - state.time) / 1000.0;
 
   // Update the cube position
   cube.position.x =
@@ -134,26 +190,33 @@ onMounted(() => {
     // Init the scene params
     E.apS('params', E.of(sceneParams)),
 
-    // Init the camera
+    // Init the camera and controls
     E.bind('camera', ({ renderer }) => E.of(createCamera(renderer))),
+    E.bind('controls', ({ camera }) => E.of(createOrbitControl(camera))),
+    E.bind('grid', () => E.of(createStageGrid())),
 
     // Init a default light
     E.bind('light', () => E.of(createLight())),
 
     // Create a default cube
-    E.bind('cube', () => E.of(createCube())),
+    E.bind('uniforms', () => E.of(createUniforms())),
+    E.bind('cube', ({ uniforms }) => E.of(createCube(uniforms))),
 
     // Build the scene from the camera, light and cube.
-    E.bind('scene', ({ camera, light, cube }) =>
-      E.of(createScene({ camera, light, cube })),
+    E.bind('scene', ({ camera, grid, light, cube }) =>
+      E.of(createScene({ camera, grid, light, cube })),
     ),
 
     // Init the animation state for this scene.
     E.bind('state', ({ cube }) => E.of(initState(cube))),
 
     // Define the animation function.
-    E.bind('animate', ({ state, params, renderer, scene, camera, cube }) =>
-      E.of(() => animate({ state, params, renderer, scene, camera, cube })),
+    E.bind(
+      'animate',
+      ({ state, params, renderer, scene, camera, cube, uniforms }) =>
+        E.of(() =>
+          animate({ state, params, renderer, scene, camera, cube, uniforms }),
+        ),
     ),
 
     // Set the animation loop
